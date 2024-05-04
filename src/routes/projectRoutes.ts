1bcf3d304fdbe665db802c15224f3173711a6b70
Miguel Cobian <miguelcobian99@gmail.com>
2024-05-03 21:03:20 -0600

    Feat. Endpoint to get all the tasks from a project

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -56,4 +56,12 @@ router.post('/:projectId/tasks',
     TaskController.createTask
 );
 
+router.get('/:projectId/tasks',
+    validateProjectExists,
+
+    TaskController.getProjectTasks,
+);
+
+
+
 export default router;
