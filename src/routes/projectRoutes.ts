1f9249bad59d42b539bf158d9c30638042db4d39
Miguel Cobian <miguelcobian99@gmail.com>
2024-05-03 18:16:39 -0600

    Feat. Add server validation to the projects

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,9 +1,22 @@
 import { Router } from "express";
 import { ProjectController } from "../controllers/ProjectController";
+import { body } from "express-validator";
+import { handleInputErrors } from "../middleware/validation";
 
 const router = Router();
 
-router.post('/', ProjectController.createProject);
+router.post('/',
+    body('projectName')
+        .notEmpty().withMessage('El nombre del proyecto es obligatorio'),
+    body('clientName')
+        .notEmpty().withMessage('El nombre del cliente es obligatorio'),
+    body('description')
+        .notEmpty().withMessage('La descripción del proyecto es obligatoria'),
+    handleInputErrors, //Middleware
+    ProjectController.createProject
+);
+
+
 router.get('/', ProjectController.getAllProjects);
 
 export default router;
