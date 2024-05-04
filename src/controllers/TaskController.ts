5affc8dbcc39d6ab796ae527df70d27d6a1bb2b2
Miguel Cobian <miguelcobian99@gmail.com>
2024-05-03 20:22:43 -0600

    Feat. Create tasks and assign them to a project

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -1,11 +1,26 @@
 import { Request, Response } from 'express';
+import Project from '../models/Project';
+import Task from '../models/Task';
 
 export class TaskController {
     static createTask = async (req: Request, res: Response) => {
         const { projectId } = req.params;
-        console.log(projectId);
-        try {
 
+        const project = await Project.findById(projectId);
+
+        if (!project) {
+            const error = new Error('Proyecto no encontrado');
+            return res.status(404).json({ error: error.message });
+        }
+
+        try {
+            const task = new Task(req.body);
+            task.project = project.id;
+            project.tasks.push(task.id);
+            await project.save();
+            await task.save();
+
+            res.send('Tarea creada correctamente');
         } catch (error) {
             console.log(error);
         }
