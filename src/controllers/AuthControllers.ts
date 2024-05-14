0aeb02134042445e4de10400810b110ead39a922
Miguel Cobian <miguelcobian99@gmail.com>
2024-05-13 19:22:14 -0600

    Feat. Create account endpoint, hash passwords and prevent duplicates

--- src/controllers/AuthControllers.ts
+++ src/controllers/AuthControllers.ts
@@ -1,11 +1,26 @@
-import { Request, Response } from 'express'
+import { Request, Response } from 'express';
+import bcrypt from 'bcrypt';
 import User from '../models/User';
+import { hashPassword } from '../utils/auth';
 
 export class AuthController {
 
     static createAccount = async (req: Request, res: Response) => {
         try {
+            const { password, email } = req.body;
+
+            // Prevenir duplicados
+            const userExists = await User.findOne({ email });
+            if (userExists) {
+                const error = new Error('El usuario ya está registrado');
+                return res.status(409).json({ error: error.message });
+            }
+
+            // Crea un usuario
             const user = new User(req.body);
+
+            // Hash password
+            user.password = await hashPassword(password);
             await user.save();
 
             res.send('Cuenta creada. Revisa tu email para confirmarla');
