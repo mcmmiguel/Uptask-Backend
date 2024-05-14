4c227d6d43ce81a614108977f5ea8b0a0c543baf
Miguel Cobian <miguelcobian99@gmail.com>
2024-05-13 19:05:42 -0600

    Feat. Create account endpoint

--- src/controllers/AuthControllers.ts
+++ src/controllers/AuthControllers.ts
@@ -1,9 +1,17 @@
 import { Request, Response } from 'express'
+import User from '../models/User';
 
 export class AuthController {
 
     static createAccount = async (req: Request, res: Response) => {
-        res.send('Hola auth');
+        try {
+            const user = new User(req.body);
+            await user.save();
+
+            res.send('Cuenta creada. Revisa tu email para confirmarla');
+        } catch (error) {
+            res.status(500).json({ error: 'Hubo un error' });
+        }
     }
 
 }
