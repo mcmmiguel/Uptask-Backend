3df1615427109a7329d9e7299bbcd2fcfd56f7fd
Miguel Cobian <miguelcobian99@gmail.com>
2024-05-13 18:33:18 -0600

    Chore. Add undefined as origin to solve cors issue to use postman

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import { connectDB } from './config/db';
 import projectRoutes from './routes/projectRoutes';
 import { corsConfig } from './config/cors';
 import morgan from 'morgan';
+import authRoutes from './routes/authRoutes';
 
 dotenv.config();
 
@@ -21,6 +22,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 // Routes
-app.use('/api/projects', projectRoutes)
+app.use('/api/auth', authRoutes);
+app.use('/api/projects', projectRoutes);
 
 export default app;
