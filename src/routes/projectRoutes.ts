231a333bec0cc4731cfaa6e1bfc131072d2f227b
Miguel Cobian <miguelcobian99@gmail.com>
2024-05-20 17:30:49 -0600

    Feat. Endpoint Search member by email

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -6,6 +6,7 @@ import { TaskController } from "../controllers/TaskController";
 import { projectExists } from "../middleware/project";
 import { taskBelongsToProject, taskExists } from "../middleware/task";
 import { authenticate } from "../middleware/auth";
+import { TeamMemberController } from "../controllers/TeamController";
 
 const router = Router();
 
@@ -96,6 +97,14 @@ router.post('/:projectId/tasks/:taskId/status',
     handleInputErrors,
     TaskController.updateStatus,
 );
+
+// Routes for teams
+router.post('/:projectId/team/find',
+    body('email')
+        .isEmail().toLowerCase().withMessage('E-mail no válido'),
+    handleInputErrors,
+    TeamMemberController.findMemberByEmail,
+)
 
 
 
