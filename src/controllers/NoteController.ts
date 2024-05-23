5a3af809665667999a532109afa9da9641a88e4b
Miguel Cobian <miguelcobian99@gmail.com>
2024-05-22 18:32:45 -0600

    Feat. Saving the note

--- src/controllers/NoteController.ts
+++ src/controllers/NoteController.ts
@@ -1,8 +1,22 @@
 import { Request, Response } from 'express';
-import { INote } from '../models/Note';
+import Note, { INote } from '../models/Note';
 
 export class NoteController {
-    static createNote = async (req: Request<{}, {}, { INote }>, res: Response) => {
-        const { content } = req.body.INote;
+    static createNote = async (req: Request<{}, {}, INote>, res: Response) => {
+        const { content } = req.body;
+
+        const note = new Note();
+        note.content = content;
+        note.createdBy = req.user.id;
+        note.task = req.task.id;
+
+        req.task.notes.push(note.id);
+
+        try {
+            await Promise.allSettled([req.task.save(), note.save()]);
+            res.send('Nota creada correctamente');
+        } catch (error) {
+            res.status(500).json({ error: 'Hubo un error' });
+        }
     }
 }
